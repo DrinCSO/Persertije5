dc9b43efbefc0f66d13e3fd2e7fbdc7f118b0989
DrinCSO <dkryeziu464@gmail.com>
2024-10-03 19:19:06 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,104 @@
+console.log("Goodbye");
+/*
+var a = 50 + 50;
+console.log(a);
+
+var b =20;
+var c = 30;
+var d = b/c;
+console.log(d);
+
+var e = 20;
+var f= 40;
+var w= f-e;
+console.log(e);
+
+var x = 15;
+x+=5;
+console.log(x);
+
+var r = 35;
+r-=5;
+consaole.log(r);
+*/
+/*
+var v = 5;
+var w = 5;
+console.log(v == w);
+
+var x = 6;
+var y = 7;
+console.log(x != y);
+
+var num1 = 100;
+var num2 = 10;
+console.log(num1 > num2);
+
+var num3 = 4;
+var num4 = 7;
+console.log(num3 < num4);
+
+var num5 = 123;
+var num6 = "123";
+
+console.log(num5 === num6);
+*/
+
+/*
+var firstName = 'Drin';
+var lastName = 'Kryeziu';
+
+var fullName = firstName + " " +lastName;
+
+console.log(fullName);
+
+var school = 'Digital School';
+
+console.log('Hello form ${school}');
+
+var age = 24;
+
+console.log(age >= 18 && age <= 56);
+
+var var1 = 5;
+var var2 = 6;
+
+console.log(var1 ==5 || var2 ==5);
+
+var var3 = 9;
+var var4 = 10;
+
+console.log(! (var3 == var4))
+*/
+/*
+function showMessage(){
+    alert("This message was inside of a function");
+}
+
+showMessage();
+
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+console.log(sum(25, 35));
+
+var arrowFunction = () => alert('Hello World');
+arrowFunction();
+
+*/
+
+function dsFunction(){
+    var localVar = 'Digital school';
+    alert (localVar);
+}
+
+dsFunction()
+
+//alert (localVar);
+
+function toSeconds(minutes){
+    return minutes = 60;
+}
+
+console.log(toSeconds(60));
